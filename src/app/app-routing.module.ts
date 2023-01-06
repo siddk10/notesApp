26a964bb82af5fa90b2687d88c11ded76280068c
siddk10 <kaslaysiddharth@gmail.com>
2023-01-06 20:15:25 +0530

    updated routing file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -23,7 +23,7 @@ const appRoutes: Routes = [
 @NgModule({
   declarations: [],
   imports: [
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes, { useHash: true })
   ],
   exports: [RouterModule]
 })
